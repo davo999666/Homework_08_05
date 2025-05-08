a14e6851cee6533c5944170012b0ce7f3e034173
David1991Grig <davoaleks1991@gmail.com>
2025-05-08 21:10:57 +0300

    first

--- http-text.js
+++ http-text.js
@@ -17,7 +17,9 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
      const value = req.url
     const homework = value.split('=')
-    if(homework[0]=== 'name'){
+    console.log(homework[0]);
+    // res.write(`<h1>Hello ${homework[0]}</h1>`);
+    if(homework[0] === '/name'){
         res.write(`<h1>Hello ${homework[1]}</h1>`);
     }
 
